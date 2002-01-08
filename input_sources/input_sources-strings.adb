04f9a3215040809029ba9031954158cba26ec39d
Emmanuel Briot <briot@adacore.com>
2002-01-08 11:15:03 +0000

    (Open): Add automatic detection of the encoding, as suggested in 9706-001
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/importfromcvs/trunk@11509 936e1b1b-40f2-da11-902a-00137254ae57

--- input_sources/input_sources-strings.adb
+++ input_sources/input_sources-strings.adb
@@ -1,9 +1,8 @@
 -----------------------------------------------------------------------
 --                XML/Ada - An XML suite for Ada95                   --
 --                                                                   --
---                       Copyright (C) 2001                          --
+--                       Copyright (C) 2001-2002                     --
 --                            ACT-Europe                             --
---                       Author: Emmanuel Briot                      --
 --                                                                   --
 -- This library is free software; you can redistribute it and/or     --
 -- modify it under the terms of the GNU General Public               --
@@ -30,6 +29,9 @@
 
 with Unicode;      use Unicode;
 with Unicode.CES;  use Unicode.CES;
+with Unicode.CES.Utf32;  use Unicode.CES.Utf32;
+with Unicode.CES.Utf16;  use Unicode.CES.Utf16;
+with Unicode.CES.Utf8;   use Unicode.CES.Utf8;
 
 package body Input_Sources.Strings is
 
@@ -40,11 +42,29 @@ package body Input_Sources.Strings is
    procedure Open
      (Str      : Unicode.CES.Byte_Sequence_Access;
       Encoding : Unicode.CES.Encoding_Scheme;
-      Input    : out String_Input) is
+      Input    : out String_Input)
+   is
+      BOM : Bom_Type;
    begin
       Input.Encoding := Encoding;
       Input.Buffer := Str;
       Input.Index := Input.Buffer'First;
+
+      Read_Bom (Input.Buffer.all, Input.Prolog_Size, BOM);
+      case BOM is
+         when Utf32_LE =>
+            Set_Encoding (Input, Utf32_LE_Encoding);
+         when Utf32_BE =>
+            Set_Encoding (Input, Utf32_BE_Encoding);
+         when Utf16_LE =>
+            Set_Encoding (Input, Utf16_LE_Encoding);
+         when Utf16_BE =>
+            Set_Encoding (Input, Utf16_BE_Encoding);
+         when Ucs4_BE | Ucs4_LE | Ucs4_2143 | Ucs4_3412 =>
+            raise Invalid_Encoding;
+         when Utf8_All | Unknown =>
+            Set_Encoding (Input, Utf8_Encoding);
+      end case;
    end Open;
 
    -----------
@@ -76,4 +96,5 @@ package body Input_Sources.Strings is
    begin
       return From.Index > From.Buffer'Last;
    end Eof;
+
 end Input_Sources.Strings;
