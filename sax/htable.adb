32ed50e1f7c266592e8680c38ec7387a88e28174
Emmanuel Briot <briot@adacore.com>
2002-06-06 12:37:15 +0000

    Initial revision, so that XML/Ada no longer depends on the GNAT-specific  GNAT.Spitbol. This is also slightly more efficient  (no use of Unbounded_Strings)
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/importfromcvs/trunk@11549 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ sax/htable.adb
@@ -0,0 +1,146 @@
+with Ada.Unchecked_Deallocation;
+with Interfaces; use Interfaces;
+
+package body HTable is
+
+   procedure Unchecked_Free is new Ada.Unchecked_Deallocation
+     (Element, Element_Ptr);
+   procedure Unchecked_Free is new Ada.Unchecked_Deallocation
+     (Htable_Item, Item_Ptr);
+
+   -----------
+   -- Reset --
+   -----------
+
+   procedure Reset (Hash_Table : in out HTable) is
+      Item, Tmp : Item_Ptr;
+   begin
+      for Index in Hash_Table.Table'Range loop
+         Item := Hash_Table.Table (Index);
+         while Item /= null loop
+            Free (Item.Elem.all);
+            Unchecked_Free (Item.Elem);
+            Tmp := Item;
+            Item := Item.Next;
+            Unchecked_Free (Tmp);
+         end loop;
+      end loop;
+   end Reset;
+
+   ---------
+   -- Set --
+   ---------
+
+   procedure Set (Hash_Table : in out HTable; E : Element) is
+      Index : constant Unsigned_32 :=
+        Hash (Get_Key (E)) mod Hash_Table.Size + 1;
+   begin
+      Hash_Table.Table (Index) := new Htable_Item'
+        (Elem => new Element'(E),
+         Next => Hash_Table.Table (Index));
+   end Set;
+
+   ---------
+   -- Get --
+   ---------
+
+   function Get (Hash_Table : HTable; K : Key) return Element is
+      Elmt : Item_Ptr := Hash_Table.Table
+        (Hash (K) mod Hash_Table.Size + 1);
+   begin
+      while Elmt /= null loop
+         if Equal (Get_Key (Elmt.Elem.all), K) then
+            return Elmt.Elem.all;
+         end if;
+
+         Elmt := Elmt.Next;
+      end loop;
+      return Empty_Element;
+   end Get;
+
+   ------------
+   -- Remove --
+   ------------
+
+   procedure Remove (Hash_Table : in out HTable; K : Key) is
+      Index     : constant Unsigned_32 :=
+        Hash (K) mod Hash_Table.Size + 1;
+      Elmt      : Item_Ptr := Hash_Table.Table (Index);
+      Next_Elmt : Item_Ptr;
+   begin
+      if Elmt = null then
+         return;
+
+      elsif Equal (Get_Key (Elmt.Elem.all), K) then
+         Hash_Table.Table (Index) := Elmt.Next;
+
+      else
+         loop
+            Next_Elmt :=  Elmt.Next;
+
+            if Next_Elmt = null then
+               return;
+
+            elsif Equal (Get_Key (Next_Elmt.Elem.all), K) then
+               Elmt.Next := Next_Elmt.Next;
+
+               Free (Next_Elmt.Elem.all);
+               Unchecked_Free (Next_Elmt.Elem);
+               Unchecked_Free (Next_Elmt);
+               return;
+            end if;
+
+            Elmt := Next_Elmt;
+         end loop;
+      end if;
+   end Remove;
+
+   -----------
+   -- First --
+   -----------
+
+   function First (Hash_Table : HTable) return Iterator is
+   begin
+      for Index in Hash_Table.Table'Range loop
+         if Hash_Table.Table (Index) /= null then
+            return (Index => Index,
+                    Item  => Hash_Table.Table (Index));
+         end if;
+      end loop;
+
+      return No_Iterator;
+   end First;
+
+   ----------
+   -- Next --
+   ----------
+
+   procedure Next (Hash_Table : in out HTable; Iter : in out Iterator) is
+   begin
+      pragma Assert (Iter /= No_Iterator);
+
+      Iter.Item := Iter.Item.Next;
+
+      if Iter.Item /= null then
+         return;
+      end if;
+
+      loop
+         Iter.Index := Unsigned_32'Succ (Iter.Index);
+         exit when Iter.Index > Hash_Table.Table'Last
+           or else Hash_Table.Table (Iter.Index) /= null;
+      end loop;
+
+      if Iter.Index > Hash_Table.Table'Last then
+         Iter := No_Iterator;
+      else
+         Iter.Item := Hash_Table.Table (Iter.Index);
+      end if;
+   end Next;
+
+   function Current (Iter : Iterator) return Element is
+   begin
+      return Iter.Item.Elem.all;
+   end Current;
+
+end HTable;
---
+++ sax/htable.ads
@@ -0,0 +1,77 @@
+with Interfaces;
+
+generic
+   type Element (<>) is private;
+   --  The type of element to be stored
+
+   Empty_Element : Element;
+
+   with procedure Free (Elmt : in out Element);
+   --  Free the memory used by Elmt
+
+   type Key (<>) is limited private;
+   with function Get_Key (E : Element)  return Key;
+   with function Hash    (F : Key)      return Interfaces.Unsigned_32;
+   with function Equal   (F1, F2 : Key) return Boolean;
+
+package HTable is
+
+   type HTable (Size : Interfaces.Unsigned_32) is private;
+
+   procedure Reset (Hash_Table : in out HTable);
+   --  Resets the hash table by freeing all the elements.
+
+   procedure Set (Hash_Table : in out HTable; E : Element);
+   --  Insert the element pointer in the HTable
+
+   function Get (Hash_Table : HTable; K : Key) return Element;
+   --  Returns the latest inserted element pointer with the given Key
+   --  or Empty_Element if none.
+
+   procedure Remove (Hash_Table : in out HTable; K : Key);
+   --  Removes the latest inserted element pointer associated with the
+   --  given key if any, does nothing if none.
+
+   type Iterator is private;
+   No_Iterator : constant Iterator;
+
+   function First (Hash_Table : HTable) return Iterator;
+   --  Return the first element in the table
+   --  There is no guarantee that 2 calls to this function will return the same
+   --  element.
+
+   procedure Next
+     (Hash_Table : in out HTable;
+      Iter       : in out Iterator);
+   --  Move to the next element in the htash table, that hasn't been returned
+   --  yet. All the elements in the table will eventually be visited if there
+   --  is no call to Set since the call to First.
+   --  Iter is set to No_Iterator if there is no more element in the table.
+
+   function Current (Iter : Iterator) return Element;
+   --  Return the element pointed to by Iter.
+
+private
+
+   type Element_Ptr is access Element;
+
+   type Htable_Item;
+   type Item_Ptr is access Htable_Item;
+   type Htable_Item is record
+      Elem : Element_Ptr;
+      Next : Item_Ptr;
+   end record;
+
+   type Item_Array is array (Interfaces.Unsigned_32 range <>) of Item_Ptr;
+
+   type HTable (Size : Interfaces.Unsigned_32) is record
+      Table : Item_Array (1 .. Size);
+   end record;
+
+   type Iterator is record
+      Index : Interfaces.Unsigned_32;
+      Item  : Item_Ptr;
+   end record;
+
+   No_Iterator : constant Iterator := (Interfaces.Unsigned_32'Last, null);
+end HTable;
