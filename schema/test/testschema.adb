f520ab093526dce0bc597ffb2a44856beb7cd935
Emmanuel Briot <briot@adacore.com>
2004-07-01 09:33:46 +0000

    Remove hand-coded grammar, since we now only read them from .xsd files ("-debug", "-xsd"): New command line switches
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/importfromcvs/trunk@11850 936e1b1b-40f2-da11-902a-00137254ae57

--- schema/test/testschema.adb
+++ schema/test/testschema.adb
@@ -5,130 +5,53 @@ with Input_Sources.File;    use Input_Sources.File;
 with Ada.Exceptions;        use Ada.Exceptions;
 with GNAT.IO;               use GNAT.IO;
 with Sax.Readers;           use Sax.Readers;
+with GNAT.Command_Line;     use GNAT.Command_Line;
+with GNAT.OS_Lib;           use GNAT.OS_Lib;
 
 procedure TestSchema is
-   Read : File_Input;
+   Read      : File_Input;
    My_Reader : Validating_Reader;
-   Schema : Schema_Reader;
-   Grammar : XML_Grammar;
-   Str_String : XML_Type;
-   AdaString, Strs, Strs2, Strs4 : Sequence;
-   Zip, Strs3 : Choice;
+   Schema    : Schema_Reader;
+   Grammar   : XML_Grammar;
+   Xsd_File  : String_Access := null;
+   Xml_File  : String_Access := null;
+
 begin
-   Open ("test.xsd", Read);
-   Set_Public_Id (Read, "test.xsd");
-   Set_System_Id (Read, "test.xsd");
-   Parse (Schema, Read);
-   Close (Read);
+   loop
+      case Getopt ("xsd: debug") is
+         when 'x' =>
+            Free (Xsd_File);
+            Xsd_File := new String'(Parameter);
+         when 'd' =>
+            Standard.Schema.Readers.Set_Debug_Output (True);
+            Standard.Schema.Validators.Set_Debug_Output (True);
+            Standard.Schema.Schema_Readers.Set_Debug_Output (True);
+         when others =>
+            exit;
+      end case;
+   end loop;
 
+   Xml_File := new String'(Get_Argument);
 
+   if Xsd_File /= null then
+      Open (Xsd_File.all, Read);
+      Set_Public_Id (Read, Xsd_File.all);
+      Set_System_Id (Read, Xsd_File.all);
+      Parse (Schema, Read);
+      Close (Read);
+      Grammar := Get_Grammar (Schema);
+   else
+      Initialize (Grammar);
+   end if;
 
-   --  <element name="root" type="anyType"/>
-
-   Initialize (Grammar);
-
-   Str_String := Lookup (Grammar, "string");
-
-   Register
-     (Grammar, Create_Element ("root", Lookup (Grammar, "anyType")));
-
-   --  <complexType name="AdaString">
-   --    <sequence>
-   --     <element name="size"     type="nonNegativeInteger"/>
-   --     <element name="contents" type="string" />
-   --    </sequence>
-   --  </complexType>
-   AdaString       := Create_Sequence;
-   Add_Particle (AdaString, Create_Element
-                   ("size",     Lookup (Grammar, "nonNegativeInteger")));
-   Add_Particle (AdaString,  Create_Element ("contents", Str_String));
-   Register (Grammar, Create_Type ("AdaString", AdaString));
-
-   --  <element name="my_string" type="AdaString" />
-   Register
-     (Grammar,
-      Create_Element ("my_string", Lookup (Grammar, "AdaString")));
-
-   --  <complexType name="zip">
-   --    <choice>
-   --     <element name="zipcode" type="string"/>
-   --     <element name="code_postal" type="string"/>
-   --    </choice>
-   --  </complexType>
-   Zip := Create_Choice (Min_Occurs => 1, Max_Occurs => 2);
-   Add_Particle (Zip, Create_Element ("zipcode", Str_String));
-   Add_Particle (Zip, Create_Element ("code_postal", Str_String));
-   Register (Grammar, Create_Type ("zip", Zip));
-
-   --  <element name="zip" type="zip" />
-   Register (Grammar, Create_Element ("zip", Lookup (Grammar, "zip")));
-
-   --  <complexType name="strs">
-   --    <sequence>
-   --     <sequence>
-   --       <element name="str" type="string"/>
-   --       <element name="str2" type="string"/>
-   --     </sequence>
-   --     <element name="str" type="string"/>
-   --    </sequence>
-   --  </complexType>
-   Strs := Create_Sequence;
-   Set_Debug_Name (Strs, "strs outer");
-   Strs2 := Create_Sequence;
-   Set_Debug_Name (Strs2, "strs inner");
-   Add_Particle (Strs2, Create_Element ("str", Str_String));
-   Add_Particle (Strs2, Create_Element ("str2", Str_String));
-   Add_Particle (Strs, Strs2);
-   Add_Particle (Strs, Create_Element ("str", Str_String));
-   Register (Grammar, Create_Type ("strs", Strs));
-
-   --  <element name="strs" type="strs" />
-   Register (Grammar, Create_Element ("strs", Lookup (Grammar, "strs")));
-
-   --  <complexType name="strs2">
-   --    <choice>
-   --     <sequence>
-   --       <element name="str" type="string"/>
-   --       <element name="str2" type="string"/>
-   --     </sequence>
-   --     <element name="str3" type="string"/>
-   --    </choice>
-   --  </complexType>
-   Strs3 := Create_Choice;
-   Set_Debug_Name (Strs3, "strs2 choice");
-   Add_Particle (Strs3, Strs2);
-   Add_Particle (Strs3, Create_Element ("str3", Str_String));
-   Register (Grammar, Create_Type ("strs2", Strs3));
-
-   --  <element name="strs2" type="strs2" />
-   Register (Grammar, Create_Element ("strs2", Lookup (Grammar, "strs2")));
-
-   --  <complexType name="strs3">
-   --    <sequence>
-   --     <choice>
-   --       <element name="str" type="string"/>
-   --       <element name="str2" type="string"/>
-   --     </schoice>
-   --     <element name="str3" type="string"/>
-   --    </sequence>
-   --  </complexType>
-   Strs3 := Create_Choice;
-   Add_Particle (Strs3, Create_Element ("str", Str_String));
-   Add_Particle (Strs3, Create_Element ("str2", Str_String));
-   Strs4 := Create_Sequence;
-   Add_Particle (Strs4, Strs3);
-   Add_Particle (Strs4, Create_Element ("str3", Str_String));
-   Register (Grammar, Create_Type ("strs3", Strs4));
-
-   --  <element name="strs3" type="strs3" />
-   Register (Grammar, Create_Element ("strs3", Lookup (Grammar, "strs3")));
-
-   Set_Grammar (My_Reader, Grammar);
-   Open ("test.xml", Read);
-   Set_Public_Id (Read, "test.xml");
-   Set_System_Id (Read, "test.xml");
-   Parse (My_Reader, Read);
-   Close (Read);
+   if Xml_File.all /= "" then
+      Set_Grammar (My_Reader, Grammar);
+      Open (Xml_File.all, Read);
+      Set_Public_Id (Read, Xml_File.all);
+      Set_System_Id (Read, Xml_File.all);
+      Parse (My_Reader, Read);
+      Close (Read);
+   end if;
 
 exception
    when E : XML_Validation_Error | XML_Fatal_Error =>
