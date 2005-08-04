589ff59df2e2ebecda4521e593ece1dc22e3d457
Emmanuel Briot <briot@adacore.com>
2005-08-04 13:38:31 +0000

    (XML_Union, Any_XML_Simple_Type): Moved to another package to keep the  interface simpler and limit dependencies (E802-003)
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/importfromcvs/trunk@12061 936e1b1b-40f2-da11-902a-00137254ae57

--- schema/schema-validators.ads
+++ schema/schema-validators.ads
@@ -358,14 +358,11 @@ package Schema.Validators is
    -- Unions --
    ------------
 
-   type XML_Union_Record is new XML_Validator_Record with private;
-   type XML_Union is access all XML_Union_Record'Class;
-
-   function Create_Union return XML_Union;
+   function Create_Union return XML_Validator;
    --  Create a new empty union
 
    procedure Add_Union
-     (Validator : access XML_Union_Record; Part : XML_Type);
+     (Validator : access XML_Validator_Record'Class; Part : XML_Type);
    --  Add a new element to the union in Validator
 
    --------------
@@ -1288,50 +1285,6 @@ private
    function Can_Be_Empty
      (Group : access XML_All_Record) return Boolean;
    --  See doc for inherited subprograms
-
-   -------------------------------
-   -- Any_Simple_XML_Validator --
-   -------------------------------
-
-   type Any_Simple_XML_Validator_Record is new XML_Validator_Record
-   with null record;
-   type Any_Simple_XML_Validator
-     is access all Any_Simple_XML_Validator_Record'Class;
-   --  Validates a "SimpleType" XML datatype, ie accepts any contents but
-   --  elements and attributes
-
-   procedure Validate_Start_Element
-     (Validator              : access Any_Simple_XML_Validator_Record;
-      Local_Name             : Unicode.CES.Byte_Sequence;
-      Namespace_URI          : Unicode.CES.Byte_Sequence;
-      NS                     : XML_Grammar_NS;
-      Data                   : Validator_Data;
-      Schema_Target_NS       : XML_Grammar_NS;
-      Element_Validator      : out XML_Element);
-   procedure Validate_End_Element
-     (Validator  : access Any_Simple_XML_Validator_Record;
-      Local_Name : Unicode.CES.Byte_Sequence;
-      Data       : Validator_Data);
-   procedure Check_Content_Type
-     (Validator        : access Any_Simple_XML_Validator_Record;
-      Should_Be_Simple : Boolean);
-   function Get_Facets_Description
-     (Validator : access Any_Simple_XML_Validator_Record)
-      return Facets_Description;
-   --  See doc from inherited subprograms
-
-   ---------------
-   -- XML_Union --
-   ---------------
-
-   type XML_Union_Record is new Any_Simple_XML_Validator_Record with record
-      Unions : Particle_List := Empty_Particle_List;
-   end record;
-   procedure Validate_Characters
-     (Union         : access XML_Union_Record;
-      Ch            : Unicode.CES.Byte_Sequence;
-      Empty_Element : Boolean);
-   --  See doc from inherited subprograms
 
    procedure Debug_Output (Str : String);
    pragma Inline (Debug_Output);
