025685b6189702e28a7acbfb6b0636f182477c1b
Emmanuel Briot <briot@adacore.com>
2005-09-23 15:26:57 +0000

    Fix the way we put the new menu before "About"
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/importfromcvs/trunk@12196 936e1b1b-40f2-da11-902a-00137254ae57

--- distrib/xmlada_gps.py
+++ distrib/xmlada_gps.py
@@ -4,10 +4,13 @@ import GPS
 
 GPS.parse_xml ("""
    <doc_path>share/doc/xmlada</doc_path>
+   <submenu before="About">
+      <title>/Help/XML Ada</title>
+   </submenu>
    <documentation_file>
       <name>xml.html</name>
       <descr>XML/Ada User's Guide</descr>
       <category>XMLAda</category>
-      <menu before="About">/Help/XML Ada/XML Ada User's Guide</menu>
+      <menu>/Help/XML Ada/XML Ada User's Guide</menu>
    </documentation_file>
 """)
