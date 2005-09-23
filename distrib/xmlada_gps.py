ca2cebce097bc4cd11b8b9744ae6b19e585bd0f9
Emmanuel Briot <briot@adacore.com>
2005-09-23 14:45:06 +0000

    Initial revision of the GPS customization file (E923-010)
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/importfromcvs/trunk@12194 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ distrib/xmlada_gps.py
@@ -0,0 +1,13 @@
+## This file provides the integration of XML/Ada into GPS
+
+import GPS
+
+GPS.parse_xml ("""
+   <doc_path>share/doc/xmlada</doc_path>
+   <documentation_file>
+      <name>xml.html</name>
+      <descr>XML/Ada User's Guide</descr>
+      <category>XMLAda</category>
+      <menu>/Help/XML Ada/XML Ada User's Guide</menu>
+   </documentation_file>
+""")
