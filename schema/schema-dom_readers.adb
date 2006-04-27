cfbbc704d5f284d3e33e09b82328dc546910b178
Emmanuel Briot <briot@adacore.com>
2006-04-27 13:30:49 +0000

    Initial revision
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/importfromcvs/trunk@12320 936e1b1b-40f2-da11-902a-00137254ae57

---
+++ schema/schema-dom_readers.adb
@@ -0,0 +1,256 @@
+-----------------------------------------------------------------------
+--                XML/Ada - An XML suite for Ada95                   --
+--                                                                   --
+--                       Copyright (C) 2006                          --
+--                            AdaCore                                --
+--                                                                   --
+-- This library is free software; you can redistribute it and/or     --
+-- modify it under the terms of the GNU General Public               --
+-- License as published by the Free Software Foundation; either      --
+-- version 2 of the License, or (at your option) any later version.  --
+--                                                                   --
+-- This library is distributed in the hope that it will be useful,   --
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of    --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details.                          --
+--                                                                   --
+-- You should have received a copy of the GNU General Public         --
+-- License along with this library; if not, write to the             --
+-- Free Software Foundation, Inc., 59 Temple Place - Suite 330,      --
+-- Boston, MA 02111-1307, USA.                                       --
+--                                                                   --
+-- As a special exception, if other files instantiate generics from  --
+-- this unit, or you link this unit with other files to produce an   --
+-- executable, this  unit  does not  by itself cause  the resulting  --
+-- executable to be covered by the GNU General Public License. This  --
+-- exception does not however invalidate any other reasons why the   --
+-- executable file  might be covered by the  GNU Public License.     --
+-----------------------------------------------------------------------
+
+with Sax.Attributes;       use Sax.Attributes;
+with Unicode;              use Unicode;
+with Unicode.CES;          use Unicode.CES;
+with DOM.Core.Attrs;       use DOM.Core.Attrs;
+with DOM.Core.Nodes;       use DOM.Core.Nodes;
+with DOM.Core.Documents;   use DOM.Core.Documents;
+with DOM.Core.Elements;    use DOM.Core.Elements;
+with DOM.Core.Character_Datas; use DOM.Core.Character_Datas;
+
+package body Schema.Dom_Readers is
+
+   --------------------
+   -- Start_Document --
+   --------------------
+
+   procedure Start_Document (Handler : in out Tree_Reader) is
+      Implementation : DOM_Implementation;
+   begin
+      Handler.Tree := Create_Document (Implementation);
+      Handler.Current_Node := Handler.Tree;
+   end Start_Document;
+
+   -------------------
+   -- Start_Element --
+   -------------------
+
+   procedure Start_Element
+     (Handler       : in out Tree_Reader;
+      Namespace_URI : Unicode.CES.Byte_Sequence := "";
+      Local_Name    : Unicode.CES.Byte_Sequence := "";
+      Qname         : Unicode.CES.Byte_Sequence := "";
+      Atts          : Sax.Attributes.Attributes'Class)
+   is
+      Att, Att2 : Attr;
+      pragma Warnings (Off, Local_Name);
+      pragma Warnings (Off, Att2);
+   begin
+      Handler.Current_Node := Append_Child
+        (Handler.Current_Node,
+         Create_Element_NS (Handler.Tree,
+                            Namespace_URI => Namespace_URI,
+                            Qualified_Name => Qname));
+
+      --  Insert the attributes in the right order.
+      for J in 0 .. Get_Length (Atts) - 1 loop
+         Att := Create_Attribute_NS
+           (Handler.Tree,
+            Namespace_URI  => Get_URI (Atts, J),
+            Qualified_Name => Get_Qname (Atts, J));
+         Set_Value (Att, Get_Value (Atts, J));
+         Att2 := Set_Attribute_Node (Handler.Current_Node, Att);
+         if Get_Type (Atts, J) = Id then
+            Set_Id_Attribute_Node (Handler.Current_Node, Att, Is_Id => True);
+         end if;
+      end loop;
+   end Start_Element;
+
+   -----------------
+   -- End_Element --
+   -----------------
+
+   procedure End_Element
+     (Handler : in out Tree_Reader;
+      Namespace_URI : Unicode.CES.Byte_Sequence := "";
+      Local_Name    : Unicode.CES.Byte_Sequence := "";
+      Qname         : Unicode.CES.Byte_Sequence := "")
+   is
+      pragma Warnings (Off, Namespace_URI);
+      pragma Warnings (Off, Local_Name);
+      pragma Warnings (Off, Qname);
+   begin
+      Handler.Current_Node := Parent_Node (Handler.Current_Node);
+   end End_Element;
+
+   ----------------
+   -- Characters --
+   ----------------
+
+   procedure Characters
+     (Handler : in out Tree_Reader; Ch : Unicode.CES.Byte_Sequence)
+   is
+      Tmp : Node;
+      pragma Unreferenced (Tmp);
+   begin
+      --  If previous child is already a text node, we should just concatenate
+      --  the two, as required in DOM specifications (in Text node description)
+      if Last_Child (Handler.Current_Node) /= null
+        and then Node_Type (Last_Child (Handler.Current_Node)) = Text_Node
+      then
+         Append_Data (Last_Child (Handler.Current_Node), Ch);
+      else
+         Tmp := Append_Child
+           (Handler.Current_Node, Create_Text_Node (Handler.Tree, Ch));
+      end if;
+   end Characters;
+
+   -------------
+   -- Comment --
+   -------------
+
+   procedure Comment
+     (Handler : in out Tree_Reader;
+      Comment : Unicode.CES.Byte_Sequence)
+   is
+      Tmp : Node;
+      pragma Unreferenced (Tmp);
+   begin
+      Tmp := Append_Child
+        (Handler.Current_Node, Create_Comment (Handler.Tree, Comment));
+   end Comment;
+
+   --------------------------
+   -- Ignorable_Whitespace --
+   --------------------------
+
+   procedure Ignorable_Whitespace
+     (Handler : in out Tree_Reader; Ch : Unicode.CES.Byte_Sequence)
+   is
+      Tmp : Node;
+      pragma Unreferenced (Tmp);
+   begin
+      --  Ignore these white spaces at the toplevel
+      if Handler.Current_Node /= Handler.Tree then
+         Tmp := Append_Child
+           (Handler.Current_Node, Create_Text_Node (Handler.Tree, Ch));
+      end if;
+   end Ignorable_Whitespace;
+
+   ----------------------------
+   -- Processing_Instruction --
+   ----------------------------
+
+   procedure Processing_Instruction
+     (Handler : in out Tree_Reader;
+      Target  : Unicode.CES.Byte_Sequence;
+      Data    : Unicode.CES.Byte_Sequence)
+   is
+      Tmp : Node;
+      pragma Unreferenced (Tmp);
+   begin
+      if not Handler.In_DTD then
+         Tmp := Append_Child
+           (Handler.Current_Node,
+            Create_Processing_Instruction (Handler.Tree, Target, Data));
+      end if;
+   end Processing_Instruction;
+
+   --------------
+   -- Get_Tree --
+   --------------
+
+   function Get_Tree (Read : Tree_Reader) return Document is
+   begin
+      return Read.Tree;
+   end Get_Tree;
+
+   ---------------
+   -- Start_DTD --
+   ---------------
+
+   procedure Start_DTD
+     (Handler   : in out Tree_Reader;
+      Name      : Unicode.CES.Byte_Sequence;
+      Public_Id : Unicode.CES.Byte_Sequence := "";
+      System_Id : Unicode.CES.Byte_Sequence := "")
+   is
+      pragma Warnings (Off, Name);
+      pragma Warnings (Off, Public_Id);
+      pragma Warnings (Off, System_Id);
+   begin
+      Handler.In_DTD := True;
+   end Start_DTD;
+
+   -------------
+   -- End_DTD --
+   -------------
+
+   procedure End_DTD (Handler : in out Tree_Reader) is
+   begin
+      Handler.In_DTD := False;
+   end End_DTD;
+
+   -----------
+   -- Error --
+   -----------
+
+   procedure Error
+     (Handler : in out Tree_Reader;
+      Except  : Sax.Exceptions.Sax_Parse_Exception'Class) is
+   begin
+      Fatal_Error (Handler, Except);
+   end Error;
+
+   -------------
+   -- Warning --
+   -------------
+
+   procedure Warning
+     (Handler : in out Tree_Reader;
+      Except : Sax.Exceptions.Sax_Parse_Exception'Class) is
+   begin
+      if Handler.Warnings_As_Error then
+         Fatal_Error (Handler, Except);
+      end if;
+   end Warning;
+
+   ----------
+   -- Free --
+   ----------
+
+   procedure Free (Read : in out Tree_Reader) is
+   begin
+      Free (Read.Tree);
+      Read.Tree := null;
+   end Free;
+
+   ----------------------------
+   -- Set_Warnings_As_Errors --
+   ----------------------------
+
+   procedure Set_Warnings_As_Errors
+     (Read : in out Tree_Reader; Warnings_As_Error : Boolean) is
+   begin
+      Read.Warnings_As_Error := Warnings_As_Error;
+   end Set_Warnings_As_Errors;
+
+end Schema.Dom_Readers;
---
+++ schema/schema-dom_readers.ads
@@ -0,0 +1,106 @@
+-----------------------------------------------------------------------
+--                XML/Ada - An XML suite for Ada95                   --
+--                                                                   --
+--                       Copyright (C) 2006                          --
+--                            AdaCore                                --
+--                                                                   --
+-- This library is free software; you can redistribute it and/or     --
+-- modify it under the terms of the GNU General Public               --
+-- License as published by the Free Software Foundation; either      --
+-- version 2 of the License, or (at your option) any later version.  --
+--                                                                   --
+-- This library is distributed in the hope that it will be useful,   --
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of    --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details.                          --
+--                                                                   --
+-- You should have received a copy of the GNU General Public         --
+-- License along with this library; if not, write to the             --
+-- Free Software Foundation, Inc., 59 Temple Place - Suite 330,      --
+-- Boston, MA 02111-1307, USA.                                       --
+--                                                                   --
+-- As a special exception, if other files instantiate generics from  --
+-- this unit, or you link this unit with other files to produce an   --
+-- executable, this  unit  does not  by itself cause  the resulting  --
+-- executable to be covered by the GNU General Public License. This  --
+-- exception does not however invalidate any other reasons why the   --
+-- executable file  might be covered by the  GNU Public License.     --
+-----------------------------------------------------------------------
+
+--  This package provides a reader that generates a DOM tree and validates
+--  the XML stream while it creates the tree.
+
+with Sax.Attributes;
+with Sax.Exceptions;
+with Schema.Readers;
+with Unicode.CES;
+with DOM.Core;             use DOM.Core;
+
+package Schema.Dom_Readers is
+
+   type Tree_Reader is new Schema.Readers.Validating_Reader with private;
+   type Tree_Reader_Access is access all Tree_Reader'Class;
+   --  Special Reader that creates a DOM tree in its callbacks.
+   --  Note that in case of a fatal error, it is your responsability to
+   --  free the tree, since it is left in the state it was when the error
+   --  was raised (for post-death analysis, if required).
+
+   function Get_Tree (Read : Tree_Reader) return Document;
+
+   procedure Free (Read : in out Tree_Reader);
+   --  Free the memory associated with the reader, in particular the tree.
+
+   procedure Set_Warnings_As_Errors
+     (Read : in out Tree_Reader; Warnings_As_Error : Boolean);
+   --  iF Warnings_As_Error is True, then all warnings will raise a fatal error
+   --  exception, just like a fatal error. Otherwise, warnings are ignored.
+
+private
+
+   type Tree_Reader is new Schema.Readers.Validating_Reader with record
+      Tree                       : Document;
+      Current_Node               : Node;
+      Internal_Encoding          : Unicode.CES.Encoding_Scheme;
+      In_DTD                     : Boolean := False;
+
+      Warnings_As_Error          : Boolean := False;
+   end record;
+
+   procedure Start_Document (Handler : in out Tree_Reader);
+   procedure Start_Element
+     (Handler       : in out Tree_Reader;
+      Namespace_URI : Unicode.CES.Byte_Sequence := "";
+      Local_Name    : Unicode.CES.Byte_Sequence := "";
+      Qname         : Unicode.CES.Byte_Sequence := "";
+      Atts          : Sax.Attributes.Attributes'Class);
+   procedure End_Element
+     (Handler : in out Tree_Reader;
+      Namespace_URI : Unicode.CES.Byte_Sequence := "";
+      Local_Name    : Unicode.CES.Byte_Sequence := "";
+      Qname         : Unicode.CES.Byte_Sequence := "");
+   procedure Characters
+     (Handler : in out Tree_Reader;
+      Ch      : Unicode.CES.Byte_Sequence);
+   procedure Ignorable_Whitespace
+     (Handler : in out Tree_Reader;
+      Ch      : Unicode.CES.Byte_Sequence);
+   procedure Processing_Instruction
+     (Handler : in out Tree_Reader;
+      Target  : Unicode.CES.Byte_Sequence;
+      Data    : Unicode.CES.Byte_Sequence);
+   procedure Start_DTD
+     (Handler   : in out Tree_Reader;
+      Name      : Unicode.CES.Byte_Sequence;
+      Public_Id : Unicode.CES.Byte_Sequence := "";
+      System_Id : Unicode.CES.Byte_Sequence := "");
+   procedure End_DTD (Handler : in out Tree_Reader);
+   procedure Comment
+     (Handler : in out Tree_Reader;
+      Comment : Unicode.CES.Byte_Sequence);
+   procedure Error
+     (Handler : in out Tree_Reader;
+      Except  : Sax.Exceptions.Sax_Parse_Exception'Class);
+   procedure Warning
+     (Handler : in out Tree_Reader;
+      Except : Sax.Exceptions.Sax_Parse_Exception'Class);
+end Schema.Dom_Readers;
