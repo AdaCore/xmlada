b33da0b9e83b2ac0f4574f82ecb14f9c877d3e3e
Emmanuel Briot <briot@adacore.com>
2006-07-12 13:14:19 +0000

    No need to explicitly create the schema-for-schema anymore
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/importfromcvs/trunk@12388 936e1b1b-40f2-da11-902a-00137254ae57

--- schema/test/testdomschema.adb
+++ schema/test/testdomschema.adb
@@ -1,5 +1,5 @@
 with Schema.Readers;        use Schema.Readers;
-with Schema.Schema_Grammar; use Schema.Schema_Grammar;
+--  with Schema.Schema_Grammar; use Schema.Schema_Grammar;
 with Schema.Schema_Readers; use Schema.Schema_Readers;
 with Schema.Validators;     use Schema.Validators;
 with Schema.Dom_Readers;    use Schema.Dom_Readers;
@@ -43,7 +43,6 @@ begin
 
    if Xsd_File /= null then
       Open (Xsd_File.all, Read);
-      Set_Validating_Grammar (Schema, Create_Schema_For_Schema);
       Parse (Schema, Read);
       Close (Read);
       Grammar := Get_Created_Grammar (Schema);
