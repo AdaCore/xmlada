dc260647275e7b8448c2abd1edd8d3fcdc012549
Emmanuel Briot <briot@adacore.com>
2007-07-30 10:36:23 +0000

    (True_Sequence, False_Sequence): new constants
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/xmlada@114608 936e1b1b-40f2-da11-902a-00137254ae57

--- sax/sax-encodings.ads
+++ sax/sax-encodings.ads
@@ -1,3 +1,31 @@
+-----------------------------------------------------------------------
+--                XML/Ada - An XML suite for Ada95                   --
+--                                                                   --
+--                    Copyright (C) 2002-2007, AdaCore               --
+--                                                                   --
+-- This library is free software; you can redistribute it and/or     --
+-- modify it under the terms of the GNU General Public               --
+-- License as published by the Free Software Foundation; either      --
+-- version 2 of the License, or (at your option) any later version.  --
+--                                                                   --
+-- This library is distributed in the hope that it will be useful,   --
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of    --
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU --
+-- General Public License for more details.                          --
+--                                                                   --
+-- You should have received a copy of the GNU General Public         --
+-- License along with this library; if not, write to the             --
+-- Free Software Foundation, Inc., 59 Temple Place - Suite 330,      --
+-- Boston, MA 02111-1307, USA.                                       --
+--                                                                   --
+-- As a special exception, if other files instantiate generics from  --
+-- this unit, or you link this unit with other files to produce an   --
+-- executable, this  unit  does not  by itself cause  the resulting  --
+-- executable to be covered by the GNU General Public License. This  --
+-- exception does not however invalidate any other reasons why the   --
+-- executable file  might be covered by the  GNU Public License.     --
+-----------------------------------------------------------------------
+
 with Unicode.CES.Utf8;
 with Unicode.CES.Utf32;
 use Unicode.CES;
@@ -76,6 +104,8 @@ package Sax.Encodings is
    Xml_Sequence           : constant Byte_Sequence := "xml";
    Xmlns_Sequence         : constant Byte_Sequence := "xmlns";
    Yes_Sequence           : constant Byte_Sequence := "yes";
+   True_Sequence          : constant Byte_Sequence := "true";
+   False_Sequence         : constant Byte_Sequence := "false";
    Vertical_Line_Sequence : constant Byte_Sequence := "|";
    Comma_Sequence         : constant Byte_Sequence := ",";
    Closing_Parenthesis_Sequence : constant Byte_Sequence := ")";
